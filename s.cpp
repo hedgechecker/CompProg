920afdcf41ab4fa0b9b96af8e96b951926889a76
Lukas <lukas_n2@gmx.de>
2025-10-22 20:23:41 +0200

    date

--- s.cpp
+++ s.cpp
@@ -4,22 +4,31 @@ int main() {
  ios::sync_with_stdio(false);
  cin.tie(nullptr);
  int T,n,m=1,l=0,r,in;
+ bitset<1000001> seen;
  vector<unsigned long long> arr(15625001);
+ vector<int> tag(15625001, -1);
+  vector<int> f(n); 
+ int curT = 0;
  cin >>T;
  while(T--){
   cin >> n;
-  vector<int> f(n);
-  fill(arr.begin(), arr.end(), 0ULL);
   m =1;l=0;
   for(r=0;r<n;++r) {
+    f.resize(n);
    cin >> f[r];
-   while(((arr[f[r]/64]) & (1ULL << (f[r]%64))) != 0){
-    arr[f[l]/64] &= ~(1ULL << (f[l]%64));
+   int idx = f[r] >> 6, bit = f[r] & 63;
+    if(tag[idx] != curT){
+        arr[idx] = 0;
+        tag[idx] = curT;
+    }
+   while(((arr[idx]) & (1ULL << (bit))) != 0){
+    arr[f[l]>>6] &= ~(1ULL << (f[l]&63));
     l++;
    } 
-   arr[f[r]/64] |= (1ULL << (f[r]%64));
+   arr[idx] |= (1ULL << (bit));
    m = max(m, r - l + 1);
   }
-  cout << m << endl;
+  cout << m << "\n";
+  curT ++;
  }
 }
